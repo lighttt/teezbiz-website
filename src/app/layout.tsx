23579c67e9ce8cc02e25d610d97b1b7d605d3bb4
lighttt <manish.eclipse@gmail.com>
2024-09-22 02:27:26 +1000

    feat - Add speed insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Kumbh_Sans } from 'next/font/google';
 import '../components/carousel/carousel.css';
@@ -34,6 +36,8 @@ export default function RootLayout({
                         <Header />
                         {children}
                         <Footer />
+                        <Analytics />
+                        <SpeedInsights />
                     </ThemeProvider>
                 </AppRouterCacheProvider>
             </body>
