22eef682927bb84f3ff70eb1a4604380463d5be2
lighttt <manish.eclipse@gmail.com>
2024-09-22 02:24:53 +1000

    feat - Update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,47 @@ import Features from './components/features';
 import HighVolumeOrderForm from './components/high_volume_order_form';
 import ProductShowcase from './components/product_showcase';
 
+export async function generateMetadata() {
+    return {
+        title: 'TeezBiz - Your Partner for Custom Apparel Printing & Brand Identity Creation',
+        description:
+            'TeezBiz specializes in premium t-shirt and hoodie printing, empowering businesses and individuals to build their brand identity through high-quality custom apparel. From design to delivery, we make it easy for you to create standout merchandise and leave a lasting impression.',
+        image: 'https://i.ibb.co/hcpc3xs/logo.png',
+        openGraph: {
+            title: 'TeezBiz - Your Partner for Custom Apparel Printing & Brand Identity Creation',
+            description:
+                'TeezBiz specializes in premium t-shirt and hoodie printing, empowering businesses and individuals to build their brand identity through high-quality custom apparel. From design to delivery, we make it easy for you to create standout merchandise and leave a lasting impression.',
+            image: 'https://i.ibb.co/hcpc3xs/logo.png',
+            images: [
+                {
+                    url: 'https://i.ibb.co/hcpc3xs/logo.png',
+                    width: 401,
+                    height: 401,
+                },
+            ],
+            type: 'website',
+            siteName: 'TeezBiz',
+        },
+        twitter: {
+            cardType: 'summary_large_image',
+            card: 'summary_large_image',
+            image: 'https://i.ibb.co/hcpc3xs/logo.png',
+            images: [
+                {
+                    url: 'https://i.ibb.co/hcpc3xs/logo.png',
+                    width: 401,
+                    height: 401,
+                },
+            ],
+            site: '@teezbiz',
+            title: 'TeezBiz - Your Partner for Custom Apparel Printing & Brand Identity Creation',
+            description:
+                'TeezBiz specializes in premium t-shirt and hoodie printing, empowering businesses and individuals to build their brand identity through high-quality custom apparel. From design to delivery, we make it easy for you to create standout merchandise and leave a lasting impression.',
+            type: 'website',
+        },
+    };
+}
+
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between ">
